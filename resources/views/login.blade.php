c82697a6e9f5b3cdf46523a4999c5d5e9d0e0dc3
Aiman <aiman@example.com>
2024-03-13 11:49:03 +0800

    test

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,10 +9,10 @@
     <p>
         <h1> Aiman </h1>
         <form>
-            <label type="email" name="email">Email</label>
+            <label type="email" name="email">Email</label><br>
             <input type="text" name="email">
             <br>
-            <label type="password" name="password" >Password</label>
+            <label type="password" name="password" >Password</label><br>
             <input name="password" type="text">
         </form>
         
